e045d0cb2ba295d21246e04c33066e66ec1dcac0
1851010123 <1851010123thao@ou.edu.vn>
2021-10-18 14:10:42 +0700

    cap nhat 2

---
+++ UnitTest/TestCustomer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyBanHang.Controllers;
+using System.Web.Mvc;
+using QuanLyBanHang.Areas.Admin.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TestCustomer
+    {
+        KhachHangsController kh;
+        [TestMethod]
+        public void Test_DeleteCustomer_Success()
+        {
+            kh = new KhachHangsController();
+            Assert.AreEqual(3, kh.DSKH().Count);
+            kh.Delete(20);
+            Assert.AreEqual(2, kh.DSKH().Count);
+        }
+    }
+}
---
+++ UnitTest/TestRegister.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuanLyBanHang.Controllers;
+using System.Web.Mvc;
+using QuanLyBanHang;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    class TestRegister
+    {
+        [TestMethod]
+        public void SignUp() {
+        LoginController lg = new LoginController();
+
+         
+        }
+    }
+}
